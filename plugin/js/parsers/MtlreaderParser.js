ac966f5d348ed87b485b62d4f10768c70927a770
k4noe <16418804+k4noe@users.noreply.github.com>
2026-03-03 16:06:16 +0700

    Fixing mtlreader parser
    Now mtl reader is working

--- plugin/js/parsers/MtlreaderParser.js
+++ plugin/js/parsers/MtlreaderParser.js
@@ -8,12 +8,43 @@ class MtlreaderParser extends Parser {
     }
 
     async getChapterUrls(dom) {
-        let menu = dom.querySelector("table.table-responsive");
-        return util.hyperlinksToChapterList(menu);
+        return this.getChapterUrlsFromMultipleTocPages(dom,
+            this.extractPartialChapterList,
+            this.getUrlsOfTocPages,
+            chapterUrlsUI
+        );
+    }
+    extractPartialChapterList(dom) {
+        let links = [...dom.querySelectorAll("a.chapter-link")];
+        if (links.length === 0) {
+            return [];
+        }
+        return links.map(a => util.hyperLinkToChapter(a));
     }
 
+    getUrlsOfTocPages(dom) {
+        let urls = [];
+        let seen = new Set();
+        dom.querySelectorAll("a.page-link").forEach(a => {
+            let href = a.href;
+            if (href && href.includes("?page=")) {
+                if (!seen.has(href)) {
+                    seen.add(href);
+                    urls.push(href);
+                }
+            }
+        });
+        // sort by page number
+        urls.sort((a, b) => {
+            let pa = parseInt(a.split("?page=")[1] || "1");
+            let pb = parseInt(b.split("?page=")[1] || "1");
+            return pa - pb;
+        });
+        return urls;
+    }
+    
     findContent(dom) {
-        return dom.querySelector("div.chapter-content");
+         return dom.querySelector("div[class~='8e9ce57f1a']"); //fix
     }
 
     extractTitleImpl(dom) {
@@ -21,14 +52,18 @@ class MtlreaderParser extends Parser {
     }
 
     findChapterTitle(dom) {
-        return dom.querySelector("div.text-center").textContent;
+        return dom.querySelector("div[class='col-md-12 text-center']").textContent.trim(); //fix
     }
 
     findCoverImageUrl(dom) {
-        return util.getFirstImgSrc(dom, "div.container");
+        return dom.querySelector("img.img-fluid.img-thumbnail.mx-auto.d-block").src;//fix
     }
 
     getInformationEpubItemChildNodes(dom) {
-        return [...dom.querySelectorAll("div#editdescription")];
+        return [...dom.querySelectorAll("div.mb-2[style*='line-height:1.6']")]; //fix
+    }
+
+    extractAuthor(dom) {
+        return dom.querySelectorAll('i.fa.fa-user')[0].nextSibling.data.replace("Author:","").trim(); //fix
     }
 }
